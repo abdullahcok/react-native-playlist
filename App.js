a1f482bb7d21c4d5024bd785d5a31020ad1a8783
Abdullah <abdullahcok@yandex.com>
2024-01-03 18:15:34 +0300

    rappers gonna search

--- App.js
+++ App.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Reac, {useState} from 'react';
+import { FlatList, SafeAreaView, StyleSheet, View } from 'react-native';
 import music_data from './music-data.json'
 import SongCard from './src/components/SongCard/SongCard';
+import SearchBar from './src/components/SearchBar';
 
 function App() {
+  const [list, setList] = useState(music_data)
 
   const renderSong = ({item}) => <SongCard song={item} />
   const renderSeperator = () => <View style={styles.seperator}></View>
+  const handleSearch = text => {
+    const filteredList = music_data.filter(song => {
+      const searchedText = text.toLowerCase()
+      const currentTitle = song.title.toLowerCase()
+
+      return currentTitle.indexOf(searchedText) > -1
+    })
+
+    setList(filteredList)
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
+        <SearchBar onSearch={handleSearch}/>
         <FlatList keyExtractor={item => item.id}
-                  data={music_data}
+                  data={list}
                   renderItem={renderSong}
                   ItemSeparatorComponent={renderSeperator}
         />
---
+++ src/components/SearchBar/SearchBar.styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    container: {
+        marginTop: 50,
+        padding: 10,
+        backgroundColor: '#eceff1',
+        borderRadius: 15,
+    },
+    text_input: {
+        fontSize: 20,
+        paddingLeft: 10,
+        color: '#2d2d2d'
+    }
+
+})
